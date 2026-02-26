92b98dfefb10cb3dabe9b5b095ceee0a867ad4b1
basantashrestha <basanta.shrestha@gmail.com>
2026-02-27 04:10:26 +0545

    update

--- nemt_backend/nemt/settings.py
+++ nemt_backend/nemt/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t77dr07i9tpv1q8ppa*ud#!vpsz)wc&7qh&nys+36^eyy5jum^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
